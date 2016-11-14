53396ddd77aa5cbc5aaffa309ff63d7e97a4945f
Gregor <gregor@martynus.net>
2016-11-14 11:43:15 -0800

    test: mock env vars for config unit tests

--- test/unit/config-get-test.js
+++ test/unit/config-get-test.js
@@ -1,9 +1,29 @@
+const defaultsDeep = require('lodash').defaultsDeep
+const proxyquire = require('proxyquire').noCallThru().noPreserveCache()
+const simple = require('simple-mock')
 const test = require('tap').test
 
-const getConfig = require('../../lib/config/get')
+const envStub = {
+  saucelabs: {
+    connect: {
+      tunnelIdentifier: 'tunnelIdentifier'
+    },
+
+    webdriver: {
+      build: 'tunnelIdentifier',
+      desiredCapabilities: {
+        'tunnel-identifier': 'tunnelIdentifier'
+      }
+    }
+  }
+}
 
 test('config', (group) => {
   group.test('no options', (t) => {
+    const getConfig = proxyquire('../../lib/config/get', {
+      './get-env': simple.stub().returnWith(envStub)
+    })
+
     getConfig((error, config) => {
       t.error(error)
       t.end()
@@ -11,6 +31,10 @@ test('config', (group) => {
   })
 
   group.test('defaults', (t) => {
+    const getConfig = proxyquire('../../lib/config/get', {
+      './get-env': simple.stub().returnWith(envStub)
+    })
+
     getConfig(undefined, (error, config) => {
       t.error(error)
       t.equal(config.runner, 'selenium')
@@ -20,6 +44,10 @@ test('config', (group) => {
   })
 
   group.test('options.client & saucelabs.{username|accessKey}', (t) => {
+    const getConfig = proxyquire('../../lib/config/get', {
+      './get-env': simple.stub().returnWith(envStub)
+    })
+
     getConfig({
       client: 'saucelabs:opera:12:linux',
       saucelabs: {
@@ -36,7 +64,32 @@ test('config', (group) => {
     })
   })
 
+  group.test('CLIENT=selenium:phantomjs', (t) => {
+    const getConfig = proxyquire('../../lib/config/get', {
+      './get-env': simple.stub().returnWith(defaultsDeep({
+        runner: 'selenium',
+        webdriver: {
+          desiredCapabilities: {
+            browserName: 'phantomjs'
+          }
+        }
+      }, envStub))
+    })
+
+    getConfig((error, config) => {
+      simple.restore()
+      t.error(error)
+      t.equal(config.runner, 'selenium')
+      t.equal(config.webdriver.desiredCapabilities.browserName, 'phantomjs')
+      t.end()
+    })
+  })
+
   group.test('invalid runner', (t) => {
+    const getConfig = proxyquire('../../lib/config/get', {
+      './get-env': simple.stub().returnWith(envStub)
+    })
+
     getConfig({
       runner: 'invalid'
     }, (error) => {
